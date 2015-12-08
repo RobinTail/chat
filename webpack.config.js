c26cd906fa771c33f778d6911956ae55fdddf05c
Robin Tail <robin_tail@list.ru>
2015-12-08 23:15:35 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var webpack = require('webpack');
+
+module.exports = {
+    entry: ['./source/index.jsx'],
+    output: {
+        path: __dirname + '/static',
+        filename: 'index.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            }/*,
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                loaders: ['style-loader', 'css-loader', 'sass-loader']
+            }*/
+        ]
+    },
+    plugins: [
+        //new webpack.optimize.UglifyJsPlugin({minimize: true})
+    ]
+};
