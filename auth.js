11aa5478e0c48cc641c510756f91a5baea0514b4
Robin Tail <robin_tail@list.ru>
2015-12-09 19:25:52 +0300

    Add all server authentication methods

--- auth.js
+++ auth.js
@@ -6,7 +6,7 @@ var VKStrategy = require('passport-vkontakte').Strategy;
 var User = require('./user.js');
 var config = require('./config.js');
 
-module.exports = passport.use(new FBStrategy({
+passport.use(new FBStrategy({
         clientID: config.facebook.clientID,
         clientSecret: config.facebook.clientSecret,
         callbackURL: config.facebook.callbackURL
@@ -37,3 +37,101 @@ module.exports = passport.use(new FBStrategy({
         });
     }
 ));
+
+passport.use(new TWStrategy({
+        consumerKey: config.twitter.consumerKey,
+        consumerSecret: config.twitter.consumerSecret,
+        callbackURL: config.twitter.callbackURL
+    },
+    function(accessToken, refreshToken, profile, done) {
+        User.findOne({
+            oauthID: profile.id
+        }, function(err, user) {
+            if (err) {
+                console.log(err);  // handle errors!
+            } else if (user !== null) {
+                done(null, user);
+            } else {
+                user = new User({
+                    oauthID: profile.id,
+                    name: profile.displayName,
+                    created: Date.now()
+                });
+                user.save(function(err) {
+                    if (err) {
+                        console.log(err);  // handle errors!
+                    } else {
+                        console.log('saving user ' + user);
+                        done(null, user);
+                    }
+                });
+            }
+        });
+    }
+));
+
+passport.use(new GGStrategy({
+        clientID: config.google.clientID,
+        clientSecret: config.google.clientSecret,
+        callbackURL: config.google.callbackURL
+    },
+    function(request, accessToken, refreshToken, profile, done) {
+        User.findOne({
+            oauthID: profile.id
+        }, function(err, user) {
+            if (err) {
+                console.log(err);  // handle errors!
+            } else if (user !== null) {
+                done(null, user);
+            } else {
+                user = new User({
+                    oauthID: profile.id,
+                    name: profile.displayName,
+                    created: Date.now()
+                });
+                user.save(function(err) {
+                    if (err) {
+                        console.log(err);  // handle errors!
+                    } else {
+                        console.log('saving user ' + user);
+                        done(null, user);
+                    }
+                });
+            }
+        });
+    }
+));
+
+passport.use(new VKStrategy({
+        clientID: config.vk.clientID,
+        clientSecret: config.vk.clientSecret,
+        callbackURL: config.vk.callbackURL
+    },
+    function(accessToken, refreshToken, profile, done) {
+        User.findOne({
+            oauthID: profile.id
+        }, function(err, user) {
+            if (err) {
+                console.log(err);  // handle errors!
+            } else if (user !== null) {
+                done(null, user);
+            } else {
+                user = new User({
+                    oauthID: profile.id,
+                    name: profile.displayName,
+                    created: Date.now()
+                });
+                user.save(function(err) {
+                    if (err) {
+                        console.log(err);  // handle errors!
+                    } else {
+                        console.log('saving user ' + user);
+                        done(null, user);
+                    }
+                });
+            }
+        });
+    }
+));
+
+module.exports = null;
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 var session = require('express-session');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 var handlers = require('./handlers');
 var passport = require('passport');
 var auth = require('./auth');
@@ -29,8 +31,13 @@ var MongoStore = require('connect-mongo')(session);
 
 // app configuration
 app.use('/static', express.static(__dirname + '/static'));
-//app.use(express.cookieParser());
-//app.use(express.bodyParser());
+/*
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+*/
 app.use(session({
     resave: false,
     saveUninitialized: false,
@@ -54,18 +61,43 @@ passport.deserializeUser(function(id, done) {
     });
 });
 
+// handlers
 app.get('/', handlers.app);
+app.get('/chat', isAuthMiddleware, handlers.chat);
+app.get('/logout', handlers.logout);
+
+// auth handlers
 app.get('/auth/facebook', passport.authenticate('facebook'));
 app.get('/auth/facebook/callback',
     passport.authenticate('facebook', {failureRedirect: '/'}),
     handlers.authSuccess);
-app.get('/chat', isAuthMiddleware, handlers.chat);
-app.get('/logout', handlers.logout);
+app.get('/auth/twitter', passport.authenticate('twitter'));
+app.get('/auth/twitter/callback',
+    passport.authenticate('twitter', {failureRedirect: '/'}),
+    handlers.authSuccess);
+app.get('/auth/google', passport.authenticate('google', {
+        scope: [
+            'https://www.googleapis.com/auth/userinfo.profile',
+            'https://www.googleapis.com/auth/userinfo.email'
+        ]
+    }));
+app.get('/auth/google/callback',
+    passport.authenticate('google', {failureRedirect: '/'}),
+    handlers.authSuccess);
+app.get('/auth/vkontakte', passport.authenticate('vkontakte'));
+app.get('/auth/vkontakte/callback',
+    passport.authenticate('vkontakte', {failureRedirect: '/'}),
+    handlers.authSuccess);
 
+// launch server
 app.listen(8080, function() {
     console.log('Start serving');
 });
 
+/**
+ * Middleware, that checks the authentication was successful
+ * Or forces the redirect to root
+ */
 function isAuthMiddleware(req, res, next) {
     if (req.isAuthenticated()) { return next(); }
     res.redirect('/');
