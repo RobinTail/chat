99d78a227980d30872c27a3de6b2df7d13609bb7
Robin Tail <robin_tail@me.com>
2015-12-17 02:34:34 +0100

    Now test authenticated connection

--- test/socket.js
+++ test/socket.js
@@ -1,13 +1,14 @@
+var newXhr = require('socket.io-client-cookie');
 var srv = require('../server');
 var ioc = require('socket.io-client');
 var expect = require('chai').expect;
 var https = require('https');
+var testConfig = require('../test.confg');
 
 // Creates a socket.io client for the given server
-function client(srv, opts) {
+function client(srv) {
     var addr = srv.address();
     if (!addr) { addr = srv.listen().address(); }
-    if (!opts) { opts = {}; }
     var url = 'ws://localhost:' + addr.port;
     return ioc(url);
 }
@@ -21,8 +22,9 @@ describe('Chat', function() {
                 srv.close();
             });
 
-            it('Should connect in 5 seconds', function(done) {
+            it('Should connect', function(done) {
                 this.timeout(5000);
+                newXhr.setCookies('');
                 var socket = client(srv);
                 socket.on('connect', function() {
                     expect('everything').to.be.ok;
@@ -30,13 +32,54 @@ describe('Chat', function() {
                 });
             });
 
-            it('Should connect with auth error', function(done) {
+            it('Should reply with error', function(done) {
                 this.timeout(5000);
+                newXhr.setCookies('');
                 var socket = client(srv);
                 socket.on('new', function(data) {
                     expect(data.error).to.be.true;
                     done();
                 });
+            });
+
+        });
+
+        describe('Authenticated connection', function() {
+
+            afterEach(function() {
+                srv.close();
+            });
+
+            it('Should feed latest', function(done) {
+                this.timeout(12000);
+                this.isLoaded = false;
+                this.promiseRequest = function(socket) {
+                    socket.emit('latest', {});
+                    var checkLoaded = new Promise(function(resolve, reject) {
+                        setTimeout(function() {
+                            if (this.isLoaded) {
+                                resolve();
+                            } else {
+                                reject();
+                            }
+                        }.bind(this), 3000);
+                    }.bind(this));
+                    checkLoaded.catch(function() {
+                        console.log('response timeout');
+                        this.promiseRequest(socket);
+                    }.bind(this));
+                };
+                newXhr.setCookies(testConfig.cookie);
+                var socket = client(srv);
+                socket.on('connect', function() {
+                    this.promiseRequest(socket);
+                    socket.on('latest', function(data) {
+                        this.isLoaded = true;
+                        expect(data.error).to.be.false;
+                        expect(data.messages).to.be.an('array');
+                        done();
+                    }.bind(this));
+                }.bind(this));
             });
 
         });
