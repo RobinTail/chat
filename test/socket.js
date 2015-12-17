67878e7b99495acc8b431422a5fb6ecdc4b993e4
Robin Tail <robin_tail@me.com>
2015-12-17 11:31:01 +0100

    Now can test session encoding to cookie

--- test/socket.js
+++ test/socket.js
@@ -3,14 +3,62 @@ var srv = require('../server');
 var ioc = require('socket.io-client');
 var expect = require('chai').expect;
 var https = require('https');
+var signature = require('../node_modules/express-session/' +
+                'node_modules/cookie-signature');
 var testConfig = require('../test.confg');
 
-// Creates a socket.io client for the given server
+/**
+ * Creates a socket.io client for the given server
+ *
+ * @param {object} srv
+ * @returns {object}
+ */
 function client(srv) {
     var addr = srv.address();
     if (!addr) { addr = srv.listen().address(); }
     var url = 'ws://localhost:' + addr.port;
     return ioc(url);
+}
+
+/**
+ * Sign and encode session for cookie
+ *
+ * @param {string} value
+ * @returns {string}
+ */
+function encodeSession(value) {
+    return encodeURIComponent(
+        's:' + signature.sign(
+            value, testConfig.sessionSecret
+        ));
+}
+
+function cookieSession(encoded) {
+    return testConfig.cookieName + '=' + encoded;
+}
+
+/**
+ * Request latest messages in Promise
+ * Checks test.isLoaded
+ *
+ * @param {object} socket
+ * @param {object} test
+ */
+function promiseRequest(socket, test) {
+    socket.emit('latest', {});
+    var checkLoaded = new Promise(function(resolve, reject) {
+        setTimeout(function() {
+            if (test.isLoaded) {
+                resolve();
+            } else {
+                reject();
+            }
+        }, 3000);
+    });
+    checkLoaded.catch(function() {
+        console.log('response timeout');
+        promiseRequest(socket, test);
+    });
 }
 
 describe('Chat Intergation Tests', function() {
@@ -51,29 +99,18 @@ describe('Chat Intergation Tests', function() {
                 srv.close();
             });
 
+            it('Should encode session properly', function() {
+                var encoded = encodeSession(testConfig.sessionUnsigned);
+                expect(testConfig.sessionEncoded).to.be.equals(encoded);
+            }.bind(this));
+
             it('Should feed latest', function(done) {
                 this.timeout(12000);
                 this.isLoaded = false;
-                this.promiseRequest = function(socket) {
-                    socket.emit('latest', {});
-                    var checkLoaded = new Promise(function(resolve, reject) {
-                        setTimeout(function() {
-                            if (this.isLoaded) {
-                                resolve();
-                            } else {
-                                reject();
-                            }
-                        }.bind(this), 3000);
-                    }.bind(this));
-                    checkLoaded.catch(function() {
-                        console.log('response timeout');
-                        this.promiseRequest(socket);
-                    }.bind(this));
-                };
-                newXhr.setCookies(testConfig.cookie);
+                newXhr.setCookies(cookieSession(testConfig.cookieValue));
                 var socket = client(srv);
                 socket.on('connect', function() {
-                    this.promiseRequest(socket);
+                    promiseRequest(socket, this);
                     socket.on('latest', function(data) {
                         this.isLoaded = true;
                         expect(data.error).to.be.false;
