f987078826f375eea42996bc57b98c497d9d9f19
Robin Tail <robin_tail@me.com>
2015-12-18 01:29:55 +0100

    Rename test.config.js to testConfig.js + es6 export syntax

--- testConfig.js
+++ testConfig.js
@@ -1,8 +1,8 @@
-module.exports = {
+export default {
     sessionUnsigned: 'test_1234567',
-// jscs:disable maximumLineLength
+    // jscs:disable maximumLineLength
     sessionEncoded: 's%3Atest_1234567.GZ%2Bk7mix0MT7PVRrPbH63AflxPR2EsWxVJmNS3F5KNc',
-// jscs:enable maximumLineLength
+    // jscs:enable maximumLineLength
     sessionSecret: 'robintail/chat/session/secret',
     cookieName: 'connect.sid'
 };
