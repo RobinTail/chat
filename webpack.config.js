747be43fc47733745c51ed198c2036f0151a8f9c
Robin Tail <robin_tail@me.com>
2015-12-21 16:13:18 +0100

    add jsx to webpack resolved extensions

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,9 @@ module.exports = {
         path: __dirname + '/static',
         filename: 'index.js'
     },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
     module: {
         loaders: [
             {
