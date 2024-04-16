8b471f4f5ef35006b3eec2bef1f01a6130bce1d1
Robin Tail <robin_tail@me.com>
2024-04-16 23:24:57 +0200

    Removing redundant breakpoints.

--- src/vars.tsx
+++ src/vars.tsx
@@ -1,7 +1,3 @@
-export const screenXS = 479;
-export const screenSM = 767;
-export const screenMD = 991;
-
 export const wrapperXS = 100; // vw
 export const wrapperXSPadding = 10; // px
 export const wrapperSM = 95; // vw
