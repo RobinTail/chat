3757ad5d7cc64b09f9df8c45a1ff56cd191aceb4
Robin Tail <robin_tail@me.com>
2024-04-16 20:57:40 +0200

    Preview settings.

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,5 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   server: { open: true, port: 8080 },
+  preview: { open: true, port: 8080 },
 });
