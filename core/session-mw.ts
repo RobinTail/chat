89734bb2c71687955bdb03b51c117432f584bd80
Robin Tail <robin_tail@me.com>
2024-04-18 19:07:47 +0200

    Preparing for PM2 ecosystem.

--- core/session-mw.ts
+++ core/session-mw.ts
@@ -2,7 +2,7 @@ import session from "express-session";
 import { sessionSalt } from "../secrets";
 
 export const sessionMw = session({
-  secret: process.env.SESSION_SECRET || sessionSalt,
+  secret: process.env.SESSION_SALT || sessionSalt,
   resave: false,
   saveUninitialized: true,
   cookie: { secure: false },
