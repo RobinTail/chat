97db724e1f2a2909696c760684bcb4c12e64a77d
Robin Tail <robin_tail@me.com>
2024-04-19 19:59:51 +0200

    Fix Embedly API on HTTPS.

--- app/components/Embedly.tsx
+++ app/components/Embedly.tsx
@@ -80,7 +80,7 @@ export const Embedly = ({ urls, isMy }: { urls: string[]; isMy?: boolean }) => {
     }
     (async () => {
       const response = await fetch(
-        `http://api.embed.ly/1/oembed?key=${embedlyKey}&format=json&maxwidth=400&urls=${urls.slice(0, 10).map(encodeURIComponent).join(",")}`,
+        `https://api.embed.ly/1/oembed?key=${embedlyKey}&format=json&maxwidth=400&urls=${urls.slice(0, 10).map(encodeURIComponent).join(",")}`,
       );
       if (!response.ok) {
         return;
