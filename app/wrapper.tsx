46a261dd6b8dd9cc03dc934f0e8d0fe44f53c55e
Robin Tail <robin_tail@me.com>
2024-04-19 18:35:01 +0200

    Removing LG breakpoint from wrapper.

--- app/wrapper.tsx
+++ app/wrapper.tsx
@@ -3,8 +3,7 @@ import { SxProps } from "@mui/material/";
 const wrapperXS = 100; // vw
 export const wrapperXSPadding = 10; // px
 const wrapperSM = 95; // vw
-const wrapperMD = 90; // vw
-const wrapperLG = 60; // vw
+const wrapperMD = 60; // vw
 
 export const wrapperSx: SxProps = {
   mx: "auto",
@@ -12,7 +11,6 @@ export const wrapperSx: SxProps = {
     xs: `${wrapperXS}vw`,
     sm: `${wrapperSM}vw`,
     md: `${wrapperMD}vw`,
-    lg: `${wrapperLG}vw`,
   },
   px: { xs: `${wrapperXSPadding}px`, sm: "unset" },
 };
